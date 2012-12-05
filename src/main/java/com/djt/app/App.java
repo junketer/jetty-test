ca90f73c85d909d5da731d796d0fc9ab975867a1
Dan Tillin <dtillin@gmail.com>
2012-12-05 21:54:52 +0000

    Updated with static content enabled

--- src/main/java/com/djt/app/App.java
+++ src/main/java/com/djt/app/App.java
@@ -1,11 +1,12 @@
 package com.djt.app;
 
-import java.util.Enumeration;
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  * App class to set up the Jetty servlets / contexts
@@ -17,9 +18,19 @@ public class App
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new HomeServlet()),"/*");
+        context.addServlet(new ServletHolder(new HomeServlet()),"/search/*");
         context.addServlet(new ServletHolder(new TwitterFeedServlet()),"/twitter/*");
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(false);
+        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
+ 
+        resource_handler.setResourceBase("./src/main/webapp/");
+ 
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resource_handler, context, new DefaultHandler() });
+        server.setHandler(handlers);       
+
+        //context.addBean(new Date());
         server.start();
         server.join();
     }
